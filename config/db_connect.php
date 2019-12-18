538dea1eab9ec61259545f81d7811ed07e50b7e4
Alin <alyn.alyyn@gmail.com>
2019-12-18 20:04:39 +0200

    Materialize grid

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,8 +1,23 @@
 <?php
-// connect to database
-$conn = mysqli_connect('localhost', 'alin', 'alin00', 'games_db');
+//// connect to database
+//$conn = mysqli_connect('localhost', 'alin', 'alin00', 'games_db');
+//
+//// check connection
+//if (!$conn) {
+//    echo 'Connection error: ' . mysqli_connect_error();
+//}
 
-// check connection
-if (!$conn) {
-    echo 'Connection error: ' . mysqli_connect_error();
+// PDO
+
+$servername = "localhost";
+$username = "alin";
+$password = "alin00";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=games_db", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   // echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
--- index.php
+++ index.php
@@ -1,15 +1,33 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 // TODO register/login
 // TODO add/delete a game
-// TODO search
+// TODO search and sort
 include('config/db_connect.php');
 
-$sql = 'SELECT id_game, title FROM games ORDER BY id';
-$result = mysqli_query($conn, $sql);
-$games = mysqli_fetch_all($result, MYSQLI_ASSOC);
-//mysqli_free_result($result);
-mysqli_close($conn);
+//$sql = 'SELECT id_game, title FROM games ORDER BY id';
+////$result = mysqli_query($conn, $sql);
+////$games = mysqli_fetch_all($result, MYSQLI_ASSOC);
+////mysqli_free_result($result);
+////mysqli_close($conn);
 
+// PDO
+$stmt = $conn->prepare("SELECT id_game, title FROM games");
+$stmt->execute();
+
+// set the resulting array to associative
+//    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+//    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+//        echo $v;
+//    }
+//}
+//catch(PDOException $e) {
+//    echo "Error: " . $e->getMessage();
+//}
+//$conn = null;
+
+$result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
 ?>
 
 <!doctype html>
@@ -20,8 +38,9 @@ mysqli_close($conn);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"-->
+    <!--          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">-->
+    <link rel="stylesheet" href="css/materialize.css">
     <link rel="stylesheet" href="css/style.css">
     <title>BD-Games</title>
 </head>
@@ -29,7 +48,8 @@ mysqli_close($conn);
 
 <div class="content">
     <nav class="navbar navbar-expand-lg navbar-light bg-dark navbar-default fixed-top">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -54,38 +74,45 @@ mysqli_close($conn);
     </nav>
     <div class="container">
         <div class="row">
-            <div class="col-sm">
+            <?php foreach ($result
+
+            as $game) : ?>
+            <? echo $game['title']; ?>
+            <div class="col s12 m6 l3">
                 <img src="images/cyberpunk2077.jpg">
             </div>
-            <div class="col-sm">
-                <img src="images/codmw2.jpg">
-            </div>
-            <div class="col-sm">
-                <img src="images/cyberpunk2077.jpg">
-            </div>
-            <div class="col-sm">
-                <img src="images/codmw2.jpg">
-            </div>
-            <div class="col-sm">
-                <img src="images/cyberpunk2077.jpg">
-            </div>
-            <div class="col-sm">
-                <img src="images/codmw2.jpg">
-            </div>
+            <?php endforeach; ?>
         </div>
     </div>
+
 </div>
+<!--            <div class="col-sm">-->
+<!--                <img src="images/codmw2.jpg">-->
+<!--            </div>-->
+<!--            <div class="col-sm">-->
+<!--                <img src="images/cyberpunk2077.jpg">-->
+<!--            </div>-->
+<!--            <div class="col-sm">-->
+<!--                <img src="images/codmw2.jpg">-->
+<!--            </div>-->
+<!--            <div class="col-sm">-->
+<!--                <img src="images/cyberpunk2077.jpg">-->
+<!--            </div>-->
+<!--            <div class="col-sm">-->
+<!--                <img src="images/codmw2.jpg">-->
+<!--            </div>-->
 
 
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+
+<!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"-->
+<!--        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"-->
+<!--        crossorigin="anonymous"></script>-->
+<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"-->
+<!--        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"-->
+<!--        crossorigin="anonymous"></script>-->
+<!--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"-->
+<!--        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"-->
+<!--        crossorigin="anonymous"></script>-->
 </body>
 </html>
