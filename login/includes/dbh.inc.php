514db1865a3cf6aa24a56ee3276858b7a25b32b9
Alin <alin.ald99@gmail.com>
2020-06-14 22:28:54 +0300

    heroku login db

--- login/includes/dbh.inc.php
+++ login/includes/dbh.inc.php
@@ -1,12 +1,23 @@
 <?php
 
-$servername = "localhost";
-$dbUsername = "alin";
-$dbPassword = "alin00";
-$dbName = "loginsystem";
+//$servername = "localhost";
+//$dbUsername = "alin";
+//$dbPassword = "alin00";
+//$dbName = "loginsystem";
+//
+//$conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
+//
+//if(!$conn) {
+//    die("Connection failed: " . mysqli_connect_error());
+//}
 
-$conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
 
-if(!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+$url = parse_url(getenv("CLEARDB_SILVER_URL"));
+
+$servername = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+
+$conn = new PDO("mysql:host=$servername; dbname=$db", $username, $password);
---
+++ loginsystem.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 14, 2020 at 09:23 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `loginsystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `idUser` int(11) NOT NULL,
+  `uidUser` tinytext NOT NULL,
+  `emailUser` tinytext NOT NULL,
+  `pwdUser` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`idUser`, `uidUser`, `emailUser`, `pwdUser`) VALUES
+(13, 'admin', 'test@gmail.com', '$2y$10$wu70/dmK.TFoDB92L/pPDeY9A41IhTCmtEh1ec8IjuHCFlNLvaZFK');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`idUser`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
