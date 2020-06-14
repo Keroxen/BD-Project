23e89f7508d77e113351357f2c2cf1b3c80015ae
Alin <alin.ald99@gmail.com>
2020-06-14 18:25:23 +0300

    heroku

--- config/db_connect.php
+++ config/db_connect.php
@@ -22,7 +22,7 @@ $db = substr($url["path"], 1);
 
 try {
     $conn = new PDO("mysql:host=$servername; dbname=$db",  $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);;
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
